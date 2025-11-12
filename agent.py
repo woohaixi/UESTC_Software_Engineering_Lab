9fc27284bb533cf354ad8ba53c1e86fb79ed195d
woohaixi <2740077256@qq.com>
2025-11-12 11:14:47 +0800

    create retrival func logic,fill the prompts and conclude the answers

--- agent.py
+++ agent.py
@@ -28,6 +28,27 @@ class Agent():
         )
         return llm_chain.run(query)
 
+    #填充提示词并总结答案
+    def retrival_func(self,query):
+        documents=self.vdb.similarity_search_with_relevance_scores(query,k=5)
+        # print(documents)
+        # exit()
+        query_result=[doc[0].page_content for doc in documents if doc[1]>0.7]
+        prompt=PromptTemplate.from_template(RETRIVAL_PROMPT_TPL)
+        retrival_chain=LLMChain(
+            llm=get_llm_model(),
+            prompt=prompt,
+            verbose=os.getenv('VERBOSE')
+        )
+        inputs={
+            'query':query,
+            'query_result':'\n\n'.join(query_result) if len(query_result) else '没有查到'
+        }
+        return retrival_chain.run(inputs)
+
+
 if __name__=='__main__':
     agent=Agent()
-    print(agent.generic_func('你叫什么名字？'))
+    # print(agent.generic_func('你叫什么名字？'))
+    print(agent.retrival_func('介绍一下寻医问药网'))
+    print(agent.retrival_func('寻医问药网的客服电话是多少？'))
